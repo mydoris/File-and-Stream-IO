83dbfba7d763e00c452c408fdde2d57518bfae1c
suplits <supllits@gmail.com>
2012-08-16 06:31:56 -0500

    ZipStream

---
+++ ZipStream/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Ionic.Zip;
+
+namespace ZipStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stream outStream = new MemoryStream();
+            using (ZipFile zip = new ZipFile())
+            {
+                string[] files = Directory.GetFiles(@"C:\ForRobot");
+                zip.AddFiles(files,"");
+                zip.Comment = "This zip was created at " + System.DateTime.Now.ToString("G");
+                zip.Save(outStream);
+                zip.Save("ForRobot.zip");
+            }
+            
+            Console.WriteLine(outStream.Length);
+
+            Console.ReadLine(); 
+        }
+        
+    }
+}
